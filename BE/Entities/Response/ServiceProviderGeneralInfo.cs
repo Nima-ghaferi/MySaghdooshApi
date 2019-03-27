ea6806fee7f5496b0a8b42c0eac163f6d3e6422f
Nima Ghaferi <nima.ghaferi@gmail.com>
2019-03-27 13:39:49 +0430

    service providers of main page created

---
+++ BE/Entities/Response/ServiceProviderGeneralInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Entities.Response
+{
+    public class ServiceProviderGeneralInfo : ResultEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class ServiceProviderGeneralInfoList : ResultEntity
+    {
+        public ServiceProviderGeneralInfo serviceProviderList { get; set; }
+    }
+}
---
+++ BL/Business/ServiceProvider/MainPageServiceProviders.cs
@@ -0,0 +1,33 @@
+﻿using BE;
+using BE.Entities.Response;
+using BL.Business.Utility;
+using ErrorCenter;
+using ErrorCenter.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Business.ServiceProvider
+{
+    public class MainPageServiceProviders
+    {
+        public static ResultList<ServiceProviderGeneralInfo> GetMainPageProviders(int pageIndex)
+        {
+            ResultList<ServiceProviderGeneralInfo> result = null;
+            List<ServiceProviderGeneralInfo> spList = null;
+            try
+            {
+                spList = DA.Queries.Servers.SelectMainPageByPageIndex(pageIndex);
+                result = new ResultList<ServiceProviderGeneralInfo>(spList, null, true);
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error(e.Message, e);
+                result = new ResultList<ServiceProviderGeneralInfo>(null, new Error(ErrorMessage.LoadMainServiceProvidersError), false);
+            }
+            return result;
+        }
+    }
+}
