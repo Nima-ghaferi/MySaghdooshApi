2e533ea5ab04d5953faa65c489a8107a332739db
Nima Ghaferi <nima.ghaferi@gmail.com>
2019-04-02 01:11:46 +0430

    liked services created

---
+++ MySaghdooshApi/Controllers/LikedServicesController.cs
@@ -0,0 +1,29 @@
+﻿using MySaghdooshApi.Filters;
+using MySaghdooshApi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace MySaghdooshApi.Controllers
+{
+    [ValidateToken]
+    [Route("LikedServices/{pageIndex}")]
+    public class LikedServicesController : RichApi
+    {
+        public HttpResponseMessage Get(int pageIndex)
+        {
+            var headers = HttpContext.Current.Request.Headers;
+            string userMobile = headers["MobileNumber"];
+            var serviceProviders = BL.Business.ServiceProvider.SpecificServiceProvider.GetLikedServices(userMobile, pageIndex);
+            if (!serviceProviders.IsOk)
+                Response = Request.CreateResponse(HttpStatusCode.InternalServerError, serviceProviders.Error.ErrorMessage);
+            else
+                Response = Request.CreateResponse(HttpStatusCode.OK, serviceProviders);
+            return Response;
+        }
+    }
+}
